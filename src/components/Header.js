bce6a90c927ed6fdbbf6339336c81ff36bf1f147
Niraj Kumar <niraj.kumar2@publicissapient.com>
2020-05-04 16:27:03 +0530

    voice enable code merged

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,15 @@ import Button from 'react-bootstrap/Button'
 import { voiceStart } from '../service/voice';
 
 class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            voiceEnable: false,
+        }
+    }
+    
     enableVoice = () => {
+        this.setState({voiceEnable: !this.state.voiceEnable})
         voiceStart();
     }
     render() {
@@ -31,7 +39,18 @@ class Header extends Component {
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
                     </Nav>
-                    <button onClick={this.enableVoice}>Enable Voice</button>
+                    <button onClick={this.enableVoice}>
+                    {this.state.voiceEnable ? 
+                            <svg class="bi bi-mic-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M5 3a3 3 0 016 0v5a3 3 0 01-6 0V3z"/>
+                                <path fill-rule="evenodd" d="M3.5 6.5A.5.5 0 014 7v1a4 4 0 008 0V7a.5.5 0 011 0v1a5 5 0 01-4.5 4.975V15h3a.5.5 0 010 1h-7a.5.5 0 010-1h3v-2.025A5 5 0 013 8V7a.5.5 0 01.5-.5z" clip-rule="evenodd"/>
+                            </svg>
+                        :
+                            <svg class="bi bi-mic-mute-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12.734 9.613A4.995 4.995 0 0013 8V7a.5.5 0 00-1 0v1c0 .274-.027.54-.08.799l.814.814zm-2.522 1.72A4 4 0 014 8V7a.5.5 0 00-1 0v1a5 5 0 004.5 4.975V15h-3a.5.5 0 000 1h7a.5.5 0 000-1h-3v-2.025a4.973 4.973 0 002.43-.923l-.718-.719zM11 7.88V3a3 3 0 00-5.842-.963L11 7.879zM5 6.12l4.486 4.486A3 3 0 015 8V6.121z" clip-rule="evenodd"/>
+                                <path stroke="#000" d="M2 1l12 12"/>
+                            </svg>}
+                    </button>
                 </Navbar.Collapse>
             </Navbar>
         )
